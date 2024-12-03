87e4a03620486a9e48e072c7bcba55f95d8de370
Albert-Song443 <peiyan_song@brown.edu>
2024-12-02 20:59:51 -0500

    fix learning rate

--- unet/hyperparameters.py
+++ unet/hyperparameters.py
@@ -11,7 +11,7 @@ A critical parameter that can dramatically affect whether training
 succeeds or fails. The value for this depends significantly on which
 optimizer is used. Refer to the default learning rate parameter
 """
-learning_rate = 1e-3
+learning_rate = 1e-4
 
 
 """
